db5bb88add8c9a1a5c54229656ca60416d7d0da6
Ben Thomasson <bthomass@redhat.com>
2022-11-22 09:29:01 -0500

    Add models design

--- tools/convert_sa_to_django.py
+++ tools/convert_sa_to_django.py
@@ -4,7 +4,7 @@
 
 """
 Usage:
-    convert_sa_to_django [options]
+    convert_sa_to_django [options] <sa-design>
 
 Options:
     -h, --help        Show this page
@@ -14,8 +14,56 @@ Options:
 from docopt import docopt
 import logging
 import sys
+import yaml
 
 logger = logging.getLogger('convert_sa_to_django')
+
+field_map = {
+    "BOOLEAN": "BooleanField",
+    "DATE": "DateField",
+    "DATETIME": "DateTimeField",
+    "DECIMAL": "DecimalField",
+    "FLOAT": "FloatField",
+    "INTEGER": "IntegerField",
+    "NUMERIC": "NumericField",
+    "SMALLINT": "SmallIntegerField",
+    "TEXT": "TextField",
+    "TIME": "TimeField",
+    "VARCHAR": "CharField",
+    "TIMESTAMP": "DateTimeField",
+    "UUID": "UUIDField",
+    "JSON": "JSONField",
+    "JSONB": "JSONField",
+}
+
+def camelcase(s):
+    return ''.join(x.capitalize() or '_' for x in s.split('_'))
+
+def convert_field_type(field):
+    if field['type'] in field_map:
+        field['type'] = field_map[field['type']]
+    elif "VARCHAR" in field['type']:
+        field['len'] = int(field['type'].split('(')[1].split(')')[0])
+        field['type'] = field_map['VARCHAR']
+    if field.get('pk'):
+        field['type'] = 'AutoField'
+
+
+
+def clean_field(field):
+    if 'ref' in field:
+        if field['ref'] is None:
+            del field['ref']
+    if 'ref_field' in field:
+        if field['ref_field'] is None:
+            del field['ref_field']
+    if 'ref' in field:
+        field['ref'] = camelcase(field['ref'])
+    if 'pk' in field:
+        if not field['pk']:
+            del field['pk']
+
+
 
 
 def main(args=None):
@@ -28,6 +76,23 @@ def main(args=None):
         logging.basicConfig(level=logging.INFO)
     else:
         logging.basicConfig(level=logging.WARNING)
+
+
+    with open(parsed_args['<sa-design>']) as f:
+        sa_design = yaml.safe_load(f)
+
+        for model in sa_design['models']:
+            for field in model['fields']:
+                convert_field_type(field)
+                clean_field(field)
+
+            model['name'] = camelcase(model['name'])
+
+
+
+        print(yaml.dump(sa_design))
+
+
     return 0
 
 
