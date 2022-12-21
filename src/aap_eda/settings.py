4679efa3ac920339dcb765e71047206e2fff9d6c
Alexander Saprykin <osapryki@redhat.com>
2022-12-21 12:18:08 +0100

    Use PostgreSQL database

--- src/aap_eda/settings.py
+++ src/aap_eda/settings.py
@@ -23,12 +23,6 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
-from pathlib import Path
-
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
-
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
@@ -90,8 +84,12 @@ WSGI_APPLICATION = "aap_eda.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "HOST": "localhost",
+        "PORT": 5432,
+        "USER": "postgres",
+        "PASSWORD": "secret",
+        "NAME": "eda",
     }
 }
 
