1e417d092311bab54e7b6a68027ea7322912f86c
hsong-rh <hsong@redhat.com>
2023-01-19 11:55:28 -0500

    Add api schema/endpoints for extra_var and playbook

--- src/aap_eda/api/serializers/project.py
+++ src/aap_eda/api/serializers/project.py
@@ -4,6 +4,16 @@ from aap_eda.core import models
 
 
 class ExtraVarSerializer(serializers.ModelSerializer):
+    name = serializers.CharField(
+        required=True,
+        help_text="Name of the extra_var",
+    )
+
+    extra_var = serializers.CharField(
+        required=True,
+        help_text="Content of the extra_var",
+    )
+
     class Meta:
         model = models.ExtraVar
         fields = "__all__"
@@ -11,6 +21,16 @@ class ExtraVarSerializer(serializers.ModelSerializer):
 
 
 class PlaybookSerializer(serializers.ModelSerializer):
+    name = serializers.CharField(
+        required=True,
+        help_text="Name of the playbook",
+    )
+
+    playbook = serializers.CharField(
+        required=True,
+        help_text="Content of the playbook",
+    )
+
     class Meta:
         model = models.Playbook
         fields = "__all__"
--- src/aap_eda/api/views/project.py
+++ src/aap_eda/api/views/project.py
@@ -1,9 +1,43 @@
+from drf_spectacular.utils import (
+    OpenApiResponse,
+    extend_schema,
+    extend_schema_view,
+)
 from rest_framework import mixins, viewsets
 
 from aap_eda.api import serializers
 from aap_eda.core import models
 
 
+@extend_schema_view(
+    retrieve=extend_schema(
+        description="Get the extra_var by its id",
+        responses={
+            200: OpenApiResponse(
+                serializers.ExtraVarSerializer,
+                description=("Return the extra_var by its id."),
+            ),
+        },
+    ),
+    list=extend_schema(
+        description="List all extra_vars",
+        responses={
+            200: OpenApiResponse(
+                serializers.ExtraVarSerializer,
+                description=("Return a list of extra_vars."),
+            ),
+        },
+    ),
+    create=extend_schema(
+        description="Create an extra_var",
+        responses={
+            201: OpenApiResponse(
+                serializers.ExtraVarSerializer,
+                description=("Return the created extra_var."),
+            ),
+        },
+    ),
+)
 class ExtraVarViewSet(
     mixins.CreateModelMixin,
     mixins.DestroyModelMixin,
@@ -11,8 +45,47 @@ class ExtraVarViewSet(
 ):
     queryset = models.ExtraVar.objects.all()
     serializer_class = serializers.ExtraVarSerializer
+    http_method_names = ["get", "post"]
 
 
+@extend_schema_view(
+    retrieve=extend_schema(
+        description="Get the playbook by its id",
+        responses={
+            200: OpenApiResponse(
+                serializers.PlaybookSerializer,
+                description=("Return the playbook by its id."),
+            ),
+        },
+    ),
+    list=extend_schema(
+        description="List all playbooks",
+        responses={
+            200: OpenApiResponse(
+                serializers.PlaybookSerializer,
+                description=("Return a list of playbooks."),
+            ),
+        },
+    ),
+    create=extend_schema(
+        description="Create a playbook",
+        responses={
+            201: OpenApiResponse(
+                serializers.PlaybookSerializer,
+                description=("Return the created playbook."),
+            ),
+        },
+    ),
+    destroy=extend_schema(
+        description="Delete an existing playbook",
+        responses={
+            204: OpenApiResponse(
+                None,
+                description=("The playbook is deleted."),
+            ),
+        },
+    ),
+)
 class PlaybookViewSet(
     mixins.CreateModelMixin,
     mixins.DestroyModelMixin,
@@ -20,3 +93,4 @@ class PlaybookViewSet(
 ):
     queryset = models.Playbook.objects.all()
     serializer_class = serializers.PlaybookSerializer
+    http_method_names = ["get", "post", "delete"]
