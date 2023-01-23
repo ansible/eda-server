82aaa9323e950f5407ee24d994092e8963b00b2e
Alexander Saprykin <osapryki@redhat.com>
2023-01-23 12:03:17 +0100

    Logging configuration (#26)

--- src/aap_eda/settings/development.py
+++ src/aap_eda/settings/development.py
@@ -1,8 +1,8 @@
 # flake8: noqa
+import os
+
+os.environ.setdefault("EDA_DEBUG", "true")
+os.environ.setdefault("EDA_SECRET_KEY", "insecure")
+os.environ.setdefault("EDA_DB_PASSWORD", "secret")
+
 from .default import *
-
-DEBUG = True
-
-SECRET_KEY = "insecure"
-
-DATABASES["default"]["PASSWORD"] = "secret"
