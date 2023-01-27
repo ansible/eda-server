87cb282aee5e54c4c84921e5d411302b652751fc
Alexander Saprykin <osapryki@redhat.com>
2023-01-27 13:59:13 +0100

    [AAP-8436] Add projects API endpoints

--- src/aap_eda/api/views/project.py
+++ src/aap_eda/api/views/project.py
@@ -1,11 +1,28 @@
+#  Copyright 2023 Red Hat, Inc.
+#
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+
 from drf_spectacular.utils import (
     OpenApiResponse,
     extend_schema,
     extend_schema_view,
 )
-from rest_framework import mixins, viewsets
+from rest_framework import mixins, status, viewsets
+from rest_framework.decorators import action
+from rest_framework.response import Response
 
-from aap_eda.api import serializers
+from aap_eda import tasks
+from aap_eda.api import exceptions as api_exc, serializers
 from aap_eda.core import models
 
 
@@ -72,4 +89,38 @@ class PlaybookViewSet(
 ):
     queryset = models.Playbook.objects.all()
     serializer_class = serializers.PlaybookSerializer
-    http_method_names = ["get"]
+
+
+class ProjectViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = models.Project.objects.all()
+    serializer_class = serializers.ProjectSerializer
+
+    @extend_schema(
+        request=serializers.ProjectCreateSerializer,
+        responses={status.HTTP_202_ACCEPTED: serializers.TaskRefSerializer},
+    )
+    def create(self, request):
+        serializer = serializers.ProjectCreateSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        data = serializer.validated_data
+
+        job = tasks.import_project.delay(**data)
+        serializer = serializers.TaskRefSerializer(
+            {"id": job.id}, context={"request": request}
+        )
+        return Response(status=status.HTTP_202_ACCEPTED, data=serializer.data)
+
+    @extend_schema(
+        responses={status.HTTP_202_ACCEPTED: serializers.TaskRefSerializer}
+    )
+    @action(methods=["post"], detail=True)
+    def sync(self, request, pk):
+        project_exists = models.Project.objects.filter(pk=pk).exists()
+        if not project_exists:
+            raise api_exc.NotFound
+
+        job = tasks.sync_project.delay(project_id=int(pk))
+        serializer = serializers.TaskRefSerializer(
+            {"id": job.id}, context={"request": request}
+        )
+        return Response(status=status.HTTP_202_ACCEPTED, data=serializer.data)
---
+++ tests/integration/api/test_project.py
@@ -0,0 +1,158 @@
+#  Copyright 2023 Red Hat, Inc.
+#
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+from typing import Any, Dict
+from unittest import mock
+
+import pytest
+from rest_framework import status
+from rest_framework.test import APIClient
+
+from aap_eda.core import models
+
+
+@pytest.mark.django_db
+def test_list_projects(client: APIClient):
+    projects = models.Project.objects.bulk_create(
+        [
+            models.Project(
+                name="test-project-01",
+                url="https://git.example.com/acme/project-01",
+                git_hash="4673c67547cf6fe6a223a9dd49feb1d5f953449c",
+            ),
+            models.Project(
+                name="test-project-02",
+                url="https://git.example.com/acme/project-02",
+                description="Project description.",
+                git_hash="06a71890b48189edc0b7afccf18285ec042ce302",
+            ),
+        ]
+    )
+    response = client.get("/eda/api/v1/projects")
+    assert response.status_code == status.HTTP_200_OK
+    for data, project in zip(response.data, projects):
+        assert_project_data(data, project)
+
+
+@pytest.mark.django_db
+def test_retrieve_project(client: APIClient):
+    project = models.Project.objects.create(
+        name="test-project-01",
+        url="https://git.example.com/acme/project-01",
+        git_hash="4673c67547cf6fe6a223a9dd49feb1d5f953449c",
+    )
+    response = client.get(f"/eda/api/v1/projects/{project.id}")
+    assert response.status_code == status.HTTP_200_OK
+    assert_project_data(response.data, project)
+
+
+@pytest.mark.django_db
+def test_retrieve_project_not_exist(client: APIClient):
+    response = client.get("/eda/api/v1/projects/42")
+    assert response.status_code == status.HTTP_404_NOT_FOUND
+
+
+@pytest.mark.django_db
+@mock.patch("aap_eda.tasks.import_project")
+def test_import_project(import_project_task: mock.Mock, client: APIClient):
+    job_id = "3677eb4a-de4a-421a-a73b-411aa502484d"
+    job = mock.Mock(id=job_id)
+    import_project_task.delay.return_value = job
+
+    response = client.post(
+        "/eda/api/v1/projects",
+        data={
+            "name": "test-project-01",
+            "url": "https://git.example.com/acme/project-01",
+        },
+    )
+    assert response.status_code == status.HTTP_202_ACCEPTED
+    assert response.data == {
+        "id": job_id,
+        "href": f"http://testserver/eda/api/v1/tasks/{job_id}",
+    }
+
+    import_project_task.delay.assert_called_once_with(
+        name="test-project-01",
+        url="https://git.example.com/acme/project-01",
+    )
+
+
+@pytest.mark.django_db
+def test_import_project_name_conflict(client: APIClient):
+    models.Project.objects.create(
+        name="test-project-01",
+        url="https://git.example.com/acme/project-01",
+        git_hash="4673c67547cf6fe6a223a9dd49feb1d5f953449c",
+    )
+    response = client.post(
+        "/eda/api/v1/projects",
+        data={
+            "name": "test-project-01",
+            "url": "https://git.example.com/acme/project-01",
+        },
+    )
+    # FIXME(cutwater): DRF serializers return HTTP 400 Bad Request
+    #   for unique violations, instead of 409 Conflict.
+    #   We need to handle this either at serializer level or global
+    #   error handler level.
+    #   See https://github.com/encode/django-rest-framework/issues/1848
+    assert response.status_code == status.HTTP_400_BAD_REQUEST
+
+
+@pytest.mark.django_db
+@mock.patch("aap_eda.tasks.sync_project")
+def test_sync_project(sync_project_task: mock.Mock, client: APIClient):
+    project = models.Project.objects.create(
+        name="test-project-01",
+        url="https://git.example.com/acme/project-01",
+        git_hash="4673c67547cf6fe6a223a9dd49feb1d5f953449c",
+    )
+
+    job_id = "3677eb4a-de4a-421a-a73b-411aa502484d"
+    job = mock.Mock(id=job_id)
+    sync_project_task.delay.return_value = job
+
+    response = client.post(f"/eda/api/v1/projects/{project.id}/sync")
+    assert response.status_code == status.HTTP_202_ACCEPTED
+    assert response.data == {
+        "id": job_id,
+        "href": f"http://testserver/eda/api/v1/tasks/{job_id}",
+    }
+
+    sync_project_task.delay.assert_called_once_with(
+        project_id=project.id,
+    )
+
+
+@pytest.mark.django_db
+def test_sync_project_not_exist(client: APIClient):
+    response = client.post("/eda/api/v1/projects/42/sync")
+    assert response.status_code == status.HTTP_404_NOT_FOUND
+
+
+DATETIME_FORMAT = "%Y-%m-%dT%H:%M:%S.%fZ"
+
+
+def assert_project_data(data: Dict[str, Any], project: models.Project):
+    # NOTE: response.data returns OrderedDict, which reduces readability of
+    #       pytest diffs.
+    assert dict(data) == {
+        "id": project.id,
+        "url": project.url,
+        "name": project.name,
+        "description": project.description,
+        "git_hash": project.git_hash,
+        "created_at": project.created_at.strftime(DATETIME_FORMAT),
+        "modified_at": project.modified_at.strftime(DATETIME_FORMAT),
+    }
