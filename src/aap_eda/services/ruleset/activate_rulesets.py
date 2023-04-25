79bcefcec1c16c1abbb397437c40b4637483c081
Ben Thomasson <bthomass@redhat.com>
2023-04-25 14:46:19 -0400

    Add function to find ports in a rulebook

--- src/aap_eda/services/ruleset/activate_rulesets.py
+++ src/aap_eda/services/ruleset/activate_rulesets.py
@@ -17,6 +17,7 @@ import uuid
 from enum import Enum
 from typing import Optional
 
+import yaml
 from django.conf import settings
 from django.utils import timezone
 
@@ -37,6 +38,43 @@ class DeploymentType(Enum):
     DOCKER = "docker"
     PODMAN = "podman"
     K8S = "k8s"
+
+
+# Move this to rulebook parser module
+def find_ports(rulebook_text: str):
+    """
+    Returns (host, port) pairs for all sources in a rulebook.
+    """
+
+    # Walk the rulebook and find ports in source parameters
+
+    # Assume the rulebook is valid if it imported
+    rulebook = yaml.safe_load(rulebook_text)
+
+    # Make a list of host, port pairs found in all sources in
+    # rulesets in a rulebook
+    found_ports = []
+
+    # Walk all rulesets in a rulebook
+    for ruleset in rulebook:
+        # Walk through all sources in a ruleset
+        for source in ruleset.get("sources", []):
+            # Remove name from source
+            if "name" in source:
+                del source['name']
+            # The first remaining key is the type and the arguments
+            source_plugin = list(source.keys())[0]
+            source_args = source[source_plugin]
+            # Get host if it exists
+            # Maybe check for "0.0.0.0" in the future
+            host = source_args.get("host")
+            # Get port if it exists
+            maybe_port = source_args.get("port")
+            # If port is an int we found a port to expose
+            if isinstance(maybe_port, int):
+                found_ports.append((host, maybe_port))
+
+    return found_ports
 
 
 class ActivateRulesets:
