8c259d867c44a7367e7a7b8d979edd542ddc0e28
Doug Donahue <57504257+ddonahue007@users.noreply.github.com>
2023-04-26 10:40:41 -0400

    [AAP-11333] [k8s] Create Service when Rulebook source has port(s) variable (#216)
    [AAP-11333] [k8s] Create Service when Rulebook source has port(s)
    variable
    
    ---------
    
    Co-authored-by: Ben Thomasson <bthomass@redhat.com>

--- src/aap_eda/services/ruleset/activate_rulesets.py
+++ src/aap_eda/services/ruleset/activate_rulesets.py
@@ -17,6 +17,7 @@ import uuid
 from enum import Enum
 from typing import Optional
 
+import yaml
 from django.conf import settings
 from django.utils import timezone
 
@@ -37,6 +38,39 @@ class DeploymentType(Enum):
     DOCKER = "docker"
     PODMAN = "podman"
     K8S = "k8s"
+
+
+# Move this to rulebook parser module
+def find_ports(rulebook_text: str):
+    """D401: Returns (host, port) pairs for all sources in a rulebook."""
+    # Walk the rulebook and find ports in source parameters
+    # Assume the rulebook is valid if it imported
+    rulebook = yaml.safe_load(rulebook_text)
+
+    # Make a list of host, port pairs found in all sources in
+    # rulesets in a rulebook
+    found_ports = []
+
+    # Walk all rulesets in a rulebook
+    for ruleset in rulebook:
+        # Walk through all sources in a ruleset
+        for source in ruleset.get("sources", []):
+            # Remove name from source
+            if "name" in source:
+                del source["name"]
+            # The first remaining key is the type and the arguments
+            source_plugin = list(source.keys())[0]
+            source_args = source[source_plugin]
+            # Get host if it exists
+            # Maybe check for "0.0.0.0" in the future
+            host = source_args.get("host")
+            # Get port if it exists
+            maybe_port = source_args.get("port")
+            # If port is an int we found a port to expose
+            if isinstance(maybe_port, int):
+                found_ports.append((host, maybe_port))
+
+    return found_ports
 
 
 class ActivateRulesets:
@@ -211,6 +245,12 @@ class ActivateRulesets:
             url=ws_url,
             ssl_verify=ssl_verify,
             activation_id=activation_instance.id,
+            ports=[
+                port
+                for _, port in find_ports(
+                    activation_instance.activation.rulebook_rulesets
+                )
+            ],
         )
 
         secret_name = None
@@ -231,6 +271,13 @@ class ActivateRulesets:
         job_spec = k8s.create_job(
             job_name=job_name, pod_template=pod_spec, ttl=30
         )
+
+        for _, port in find_ports(
+            activation_instance.activation.rulebook_rulesets
+        ):
+            k8s.create_service(
+                namespace=namespace, job_name=job_name, port=port
+            )
 
         # execute job
         k8s.run_activation_job(
