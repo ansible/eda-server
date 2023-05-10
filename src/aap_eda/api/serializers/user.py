4108bc34e56052b2ca7b7c94f56835f0cd2b4e5d
Bill Wei <bilwei@redhat.com>
2023-05-10 09:33:19 -0400

    AwxTokenSerializer should not contain the read-only token field
    Fixes AAP-12118:openapi schema issue with AwxToken

--- src/aap_eda/api/serializers/user.py
+++ src/aap_eda/api/serializers/user.py
@@ -89,13 +89,10 @@ class AwxTokenSerializer(serializers.ModelSerializer):
             "id",
             "name",
             "description",
-            "token",
             "user_id",
             "created_at",
             "modified_at",
         ]
-        read_only_fields = ["id", "user_id", "created_at", "modified_at"]
-        extra_kwargs = {"token": {"write_only": True}}
 
 
 class AwxTokenCreateSerializer(serializers.ModelSerializer):
@@ -106,4 +103,3 @@ class AwxTokenCreateSerializer(serializers.ModelSerializer):
             "description",
             "token",
         ]
-        extra_kwargs = {"token": {"write_only": True}}
