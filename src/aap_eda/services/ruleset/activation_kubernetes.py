1d4d0c16c1ff3676f3019976fa0d935837c4eb0b
Christian M. Adams <chadams@redhat.com>
2023-05-19 17:06:58 -0400

    Remove unneeded EDA_ prefix on activation pod resources settings

--- src/aap_eda/services/ruleset/activation_kubernetes.py
+++ src/aap_eda/services/ruleset/activation_kubernetes.py
@@ -73,8 +73,8 @@ class ActivationKubernetes:
             env=[client.V1EnvVar(name="ANSIBLE_LOCAL_TEMP", value="/tmp")],
             args=args,
             resources=client.V1ResourceRequirements(
-                requests=settings.EDA_ACTIVATION_POD_RESOURCE_REQUESTS,
-                limits=settings.EDA_ACTIVATION_POD_RESOURCE_LIMITS,
+                requests=settings.ACTIVATION_POD_RESOURCE_REQUESTS,
+                limits=settings.ACTIVATION_POD_RESOURCE_LIMITS,
             ),
             ports=[
                 client.V1ContainerPort(container_port=port) for port in ports
