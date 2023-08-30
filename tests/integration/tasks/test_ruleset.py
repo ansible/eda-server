e502b7db0966d5595cd170e8a164d0b4e446ce34
Hui Song <hsong@redhat.com>
2023-08-30 16:36:19 -0400

    [AAP-15287] Add transaction atomic to solve race conditions when restarting activation (#376)

--- tests/integration/tasks/test_ruleset.py
+++ tests/integration/tasks/test_ruleset.py
@@ -21,11 +21,18 @@ from django.utils import timezone
 
 from aap_eda.core import models
 from aap_eda.core.enums import ActivationStatus
-from aap_eda.tasks.ruleset import monitor_activations
+from aap_eda.services.ruleset.activate_rulesets import ActivateRulesets
+from aap_eda.tasks.ruleset import (
+    activate,
+    deactivate,
+    monitor_activations,
+    restart,
+)
 
 
 @dataclass
 class InitData:
+    user: models.User
     activation: models.Activation
     instance1: models.ActivationInstance
     instance2: models.ActivationInstance
@@ -59,6 +66,7 @@ def init_data():
     )
 
     return InitData(
+        user=user,
         activation=activation,
         instance1=instance1,
         instance2=instance2,
@@ -66,7 +74,119 @@ def init_data():
 
 
 @pytest.mark.django_db
-@mock.patch("aap_eda.tasks.ruleset.activate_rulesets.delay")
+@mock.patch("aap_eda.tasks.ruleset.logger.info")
+def test_normal_activate(info_mock: mock.Mock, init_data):
+    activation = models.Activation.objects.create(
+        name="test-activate",
+        user=init_data.user,
+    )
+
+    msg1 = (
+        f"Activating activation id: {activation.id} requested"
+        f" by {activation.user.username}"
+    )
+    msg2 = f"Task finished: activation {activation.name} starting."
+
+    with mock.patch.object(ActivateRulesets, "activate"):
+        activate(activation.id, activation.user.username)
+
+    info_mock.assert_has_calls([mock.call(msg1), mock.call(msg2)])
+
+
+@pytest.mark.django_db
+@mock.patch("aap_eda.tasks.ruleset.logger.info")
+def test_not_enabled_activate(info_mock: mock.Mock, init_data):
+    activation = models.Activation.objects.create(
+        name="test-activate",
+        user=init_data.user,
+        is_enabled=False,
+    )
+
+    msg1 = f"Activating activation id: {activation.id} requested by User"
+    msg2 = f"Activation id: {activation.id} is disabled"
+
+    with mock.patch.object(ActivateRulesets, "activate"):
+        activate(activation.id)
+
+    info_mock.assert_has_calls([mock.call(msg1), mock.call(msg2)])
+
+
+@pytest.mark.django_db
+@mock.patch("aap_eda.tasks.ruleset.logger.info")
+def test_with_deleting_activate(info_mock: mock.Mock, init_data):
+    activation = models.Activation.objects.create(
+        name="test-activate",
+        user=init_data.user,
+        status=ActivationStatus.DELETING,
+    )
+
+    msg1 = f"Activating activation id: {activation.id} requested by User"
+    msg2 = f"Activation id: {activation.id} is deleted"
+
+    with mock.patch.object(ActivateRulesets, "activate"):
+        activate(activation.id)
+
+    info_mock.assert_has_calls([mock.call(msg1), mock.call(msg2)])
+
+
+@pytest.mark.django_db
+@mock.patch("aap_eda.tasks.ruleset.logger.info")
+def test_deactivate(info_mock: mock.Mock, init_data):
+    activation = models.Activation.objects.create(
+        name="test-activate",
+        user=init_data.user,
+    )
+    msg = (
+        f"Disabling activation id: {activation.id} requested"
+        f" by {activation.user.username}"
+    )
+
+    with mock.patch("aap_eda.tasks.ruleset._perform_deactivate"):
+        deactivate(activation.id, activation.user.username)
+
+    info_mock.assert_called_once_with(msg)
+
+
+@pytest.mark.django_db
+@mock.patch("aap_eda.tasks.ruleset.logger.info")
+def test_deactivate_with_delete(info_mock: mock.Mock, init_data):
+    activation = models.Activation.objects.create(
+        name="test-activate",
+        user=init_data.user,
+    )
+    msg1 = (
+        f"Disabling activation id: {activation.id} requested"
+        f" by {activation.user.username}"
+    )
+    msg2 = f"Activation {activation.name} is deleted"
+
+    with mock.patch("aap_eda.tasks.ruleset._perform_deactivate"):
+        deactivate(activation.id, activation.user.username, True)
+
+    info_mock.assert_has_calls([mock.call(msg1), mock.call(msg2)])
+
+
+@pytest.mark.django_db
+@mock.patch("aap_eda.tasks.ruleset.logger.info")
+@mock.patch("aap_eda.tasks.ruleset.activate.delay")
+def test_restart(delay_mock: mock.Mock, info_mock: mock.Mock, init_data):
+    activation = models.Activation.objects.create(
+        name="test-activate",
+        user=init_data.user,
+    )
+    msg = (
+        f"Restarting activation id: {activation.id} requested"
+        f" by {activation.user.username}"
+    )
+
+    with mock.patch("aap_eda.tasks.ruleset._perform_deactivate"):
+        restart(activation.id, activation.user.username)
+
+    info_mock.assert_called_once_with(msg)
+
+
+@pytest.mark.django_db
+@mock.patch("aap_eda.tasks.ruleset.activate.delay")
 def test_monitor_activations_to_unknown(delay_mock: mock.Mock, init_data):
     monitor_activations()
     init_data.instance1.refresh_from_db()
