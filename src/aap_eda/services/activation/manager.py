31d3c313eb73c1d9514e24327d7c9ec9d28d8120
Alex <aizquier@redhat.com>
2023-10-11 18:36:45 +0200

    wip: working on manager-start

--- src/aap_eda/services/activation/manager.py
+++ src/aap_eda/services/activation/manager.py
@@ -1,5 +1,40 @@
 from .factory import new_container_engine, ContainerEngine
 import aap_eda.core.models as models
+from django.db import transaction
+from django.core.exceptions import ObjectDoesNotExist
+from functools import wraps
+from aap_eda.core.enums import ActivationStatus
+import typing as tp
+
+
+class ActivationStartError(Exception):
+    pass
+
+
+class HasDbInstance(tp.Protocol):
+    db_instance: tp.Any  # or you can be more specific with the type, if desired
+
+
+def run_with_lock(func: tp.Callable) -> tp.Callable:
+    @wraps(func)
+    def _run_with_lock(self: HasDbInstance, *args, **kwargs):
+        with transaction.atomic():
+            locked_instance = (
+                type(self.db_instance)
+                .objects.select_for_update()
+                .get(pk=self.db_instance.pk)
+            )
+
+            original_instance = self.db_instance
+            self.db_instance = locked_instance
+
+            try:
+                return func(self, *args, **kwargs)
+            finally:
+                self.db_instance = original_instance
+                self.db_instance.refresh_from_db()
+
+    return _run_with_lock
 
 
 class ActivationManager:
@@ -15,10 +50,31 @@ class ActivationManager:
         pass
 
     def _get_status(self):
-        db_status = None
-        pod_state = self.container_engine.get_status()
-        if db_status == pod_state:
-            return ActivationStatus()
+        pass
+
+    @run_with_lock
+    def _check_start_requirements(self):
+        disallowed_statuses = [
+            ActivationStatus.STARTING,
+            ActivationStatus.DELETING,
+        ]
+
+        if self.db_instance.status in disallowed_statuses:
+            raise ActivationStartError(
+                f"Activation is in {self.db_instance.status} state.",
+            )
+        if self.db_instance.is_enabled is False:
+            raise ActivationStartError("Activation is disabled.")
+
+        # call is_valid_activation from https://github.com/ansible/eda-server/pull/424
+        is_valid, error = is_activation_valid(self.db_instance)
+        if is_valid is False:
+            msg = f"Activation {self.db_instance.id} is not valid. {error}"
+            raise ActivationStartError(msg)
+
+        if self.db_instance.status == ActivationStatus.RUNNING:
+            # check if the container is running
+            pass
 
     def start(self):
         """Start an activation.
@@ -28,9 +84,17 @@ class ActivationManager:
         Starts the activation in an idepotent way.
         """
         # Check for the right statuses
-        # Check is_valid(activation)
+        # It may need to be encapsulated in a private method
+        try:
+            self._check_start_requirements()
+        except ObjectDoesNotExist:
+            raise ActivationStartError(
+                "The Activation instance does not exist."
+            )
         # Check for a container
-        #
+        # start a container
+        # update status
+        # update logs
 
     def restart(self):
         pass
