98aabe61e7622a7c22e0818e596d45236b79ce13
Alex <aizquier@redhat.com>
2023-10-11 20:13:11 +0200

    wip: manager-start and podman-engine

---
+++ src/aap_eda/services/activation/engine/k8s.py
@@ -0,0 +1,18 @@
+from .common import ContainerEngine
+
+
+class K8sEngine(ContainerEngine):
+    def start(self):
+        pass
+
+    def stop(self):
+        pass
+
+    def restart(self):
+        pass
+
+    def fetch_logs(self):
+        pass
+
+    def get_status(self):
+        pass
