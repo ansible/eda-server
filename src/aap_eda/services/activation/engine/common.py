d2c8d0042d93dc5704708a4d2194d0b656d8af35
Alex <aizquier@redhat.com>
2023-10-23 16:19:30 +0200

    refactor: add pydantic and missing licences (#449)

--- src/aap_eda/services/activation/engine/common.py
+++ src/aap_eda/services/activation/engine/common.py
@@ -1,7 +1,23 @@
+#  Copyright 2023 Red Hat, Inc.
+#
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+
+
 from abc import ABC, abstractmethod
-from dataclasses import dataclass
 from datetime import datetime
 from typing import Union
+
+from pydantic import BaseModel
 
 
 class LogHandler(ABC):
@@ -22,9 +38,7 @@ class LogHandler(ABC):
         pass
 
 
-# TODO: use pydantic
-@dataclass
-class AnsibleRulebookCmdLine:
+class AnsibleRulebookCmdLine(BaseModel):
     ws_url: str
     ws_ssl_verify: str
     heartbeat: int
@@ -57,16 +71,12 @@ class AnsibleRulebookCmdLine:
         return args
 
 
-# TODO: use pydantic
-@dataclass
-class Credential:
+class Credential(BaseModel):
     username: str
     secret: str
 
 
-# TODO: use pydantic
-@dataclass
-class ContainerRequest:
+class ContainerRequest(BaseModel):
     name: str  # f"eda-{activation_instance.id}-{uuid.uuid4()}"
     image_url: str  # quay.io/ansible/ansible-rulebook:main
     cmdline: AnsibleRulebookCmdLine
