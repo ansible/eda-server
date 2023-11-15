d9fbc93fdbd3a5d8a08b22514e93db63ecf241c3
Alex <aizquier@redhat.com>
2023-11-15 13:09:08 +0100

    chore: merge main into rework-activation (#514)
    Signed-off-by: Joe Shimkus <jshimkus@redhat.com>
    Co-authored-by: Hui Song <hsong@redhat.com>
    Co-authored-by: Joe Shimkus <35382397+jshimkus-rh@users.noreply.github.com>
    Co-authored-by: Doston Toirov <31990136+Dostonbek1@users.noreply.github.com>
    Co-authored-by: Bill Wei <bilwei@redhat.com>
    Co-authored-by: Madhu Kanoor <mkanoor@redhat.com>

--- src/aap_eda/api/views/activation.py
+++ src/aap_eda/api/views/activation.py
@@ -26,10 +26,7 @@ from rest_framework.decorators import action
 from rest_framework.response import Response
 
 from aap_eda.api import exceptions as api_exc, filters, serializers
-from aap_eda.api.serializers.activation import (
-    is_activation_valid,
-    parse_validation_errors,
-)
+from aap_eda.api.serializers.activation import is_activation_valid
 from aap_eda.core import models
 from aap_eda.core.enums import Action, ActivationStatus, ResourceType
 from aap_eda.tasks.orchestrator import (
@@ -84,12 +81,7 @@ class ActivationViewSet(
         serializer = serializers.ActivationCreateSerializer(
             data=request.data, context=context
         )
-        valid = serializer.is_valid()
-        if not valid:
-            error = parse_validation_errors(serializer.errors)
-            return Response(
-                {"errors": error}, status=status.HTTP_400_BAD_REQUEST
-            )
+        serializer.is_valid(raise_exception=True)
 
         response = serializer.create(serializer.validated_data)
 
@@ -298,6 +290,10 @@ class ActivationViewSet(
             activation.status_message = error
             activation.save(update_fields=["status", "status_message"])
             logger.error(f"Failed to restart {activation.name}: {error}")
+
+            return Response(
+                {"errors": error}, status=status.HTTP_400_BAD_REQUEST
+            )
 
             return Response(
                 {"errors": error}, status=status.HTTP_400_BAD_REQUEST
