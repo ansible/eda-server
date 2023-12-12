ca90ef64307c4f923df99ebb6b46ce44d08ff6e0
Doston Toirov <doston.toirov2@gmail.com>
2023-12-12 17:16:14 -0500

    update tests

--- tests/integration/api/test_extra_var.py
+++ tests/integration/api/test_extra_var.py
@@ -58,20 +58,38 @@ def test_retrieve_extra_var_not_exist(client: APIClient):
     assert response.status_code == status.HTTP_404_NOT_FOUND
 
 
+NOT_OBJECT_ERROR_MSG = "yaml is not in object format"
+NOT_YAML_JSON_ERROR_MSG = "Extra var must be in JSON or YAML format"
+
+
 @pytest.mark.parametrize(
-    "extra_var", ["John", "John, ", "[John, 3,]", '{"name": "John" - 2 }']
+    "extra_var,error_message",
+    [
+        ("John", NOT_OBJECT_ERROR_MSG),
+        ("John, ", NOT_OBJECT_ERROR_MSG),
+        ("[John, 3,]", NOT_OBJECT_ERROR_MSG),
+        ('{"name": "John" - 2 }', NOT_YAML_JSON_ERROR_MSG),
+    ],
 )
 @pytest.mark.django_db
-def test_extra_var_invalid_data(client: APIClient, extra_var):
+def test_extra_var_invalid_data(client: APIClient, extra_var, error_message):
     invalid_data = {
         "extra_var": extra_var,
     }
     response = client.post(f"{api_url_v1}/extra-vars/", data=invalid_data)
     assert response.status_code == status.HTTP_400_BAD_REQUEST
+    assert str(response.data["extra_var"][0]) == error_message
 
 
 @pytest.mark.parametrize(
-    "extra_var", ["John: Doe", "John: 2", '{"name": "John"}', '"age": 20']
+    "extra_var",
+    [
+        "John: Doe",
+        "John: 2",
+        '{"name": "John"}',
+        '"age": 20',
+        "---\nname: hello\nhosts: localhost\ngather_facts: false",
+    ],
 )
 @pytest.mark.django_db
 def test_extra_var_valid_data(client: APIClient, extra_var):
