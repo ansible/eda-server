9fb3fac2f22fe5ea481d020a189d5f94aae9325c
Doston Toirov <doston.toirov2@gmail.com>
2024-01-25 15:07:03 -0500

    Revert "Update for django-ansible-base app split (#614)"
    This reverts commit cdd12d1c5fb883fad8d13cc7553e4908d38c775d.

--- src/aap_eda/api/urls.py
+++ src/aap_eda/api/urls.py
@@ -11,7 +11,7 @@
 #  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
-from ansible_base.lib.dynamic_config.dynamic_urls import api_version_urls
+from ansible_base.urls import urls as base_urls
 from django.urls import include, path
 from drf_spectacular.views import (
     SpectacularJSONAPIView,
@@ -67,14 +67,13 @@ openapi_urls = [
 ]
 
 v1_urls = [
-    # <- Experimental LDAP Auth https://issues.redhat.com/browse/AAP-16938
-    path("", include(api_version_urls)),
-    # ->
     path("", include(openapi_urls)),
     path("auth/session/login/", views.SessionLoginView.as_view()),
     path("auth/session/logout/", views.SessionLogoutView.as_view()),
     path("users/me/", views.CurrentUserView.as_view()),
     *router.urls,
+    # Experimental LDAP Integration https://issues.redhat.com/browse/AAP-16938
+    path("", include(base_urls)),
 ]
 
 urlpatterns = [
--- src/aap_eda/settings/default.py
+++ src/aap_eda/settings/default.py
@@ -143,8 +143,8 @@ INSTALLED_APPS = [
     "drf_spectacular",
     "django_rq",
     "django_filters",
-    # Experimental LDAP Auth https://issues.redhat.com/browse/AAP-16938
-    "ansible_base.authentication",
+    # Experimental LDAP Integration https://issues.redhat.com/browse/AAP-16938
+    "ansible_base",
     # Local apps
     "aap_eda.api",
     "aap_eda.core",
@@ -156,11 +156,10 @@ MIDDLEWARE = [
     "django.middleware.locale.LocaleMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
-    # <- Experimental LDAP Auth https://issues.redhat.com/browse/AAP-16938
-    "ansible_base.authentication.middleware.AuthenticatorBackendMiddleware",
-    # ->
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    # Experimental LDAP Integration https://issues.redhat.com/browse/AAP-16938
+    "ansible_base.utils.middleware.AuthenticatorBackendMiddleware",
 ]
 
 ROOT_URLCONF = "aap_eda.urls"
@@ -438,10 +437,15 @@ AUTHENTICATION_BACKENDS = [
     "ansible_base.authentication.backend.AnsibleBaseAuth",
     "django.contrib.auth.backends.ModelBackend",
 ]
+ANSIBLE_BASE_FEATURES = {
+    "AUTHENTICATION": True,
+    "FILTERING": False,
+    "SWAGGER": False,
+}
 
-from ansible_base.lib import dynamic_config  # noqa: E402
+from ansible_base import settings  # noqa: E402
 
-dab_settings = os.path.join(
-    os.path.dirname(dynamic_config.__file__), "dynamic_settings.py"
+settings_file = os.path.join(
+    os.path.dirname(settings.__file__), "dynamic_settings.py"
 )
-include(dab_settings)
+include(settings_file)
