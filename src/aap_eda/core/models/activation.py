a668df8d69effeeaa6feb57e40f0c0467ff5f65c
Alex <aizquier@redhat.com>
2024-01-31 22:10:06 +0100

    refactor: process_fqcn and process_id POC (#634)

--- src/aap_eda/core/models/activation.py
+++ src/aap_eda/core/models/activation.py
@@ -35,6 +35,8 @@ __all__ = (
     "RulebookProcessLog",
 )
 
+from django.utils.module_loading import import_string
+
 
 class Activation(models.Model):
     class Meta:
@@ -200,15 +202,29 @@ class RulebookProcess(models.Model):
     activation_pod_id = models.TextField(null=True)
     status_message = models.TextField(null=True, default=None)
     log_read_at = models.DateTimeField(null=True)
+    parent_id = models.BigIntegerField(null=False)
+    parent_fqcn = models.TextField(null=False)
+
+    def get_parent(self):
+        try:
+            parent_model = import_string(self.parent_fqcn)
+        except ImportError:
+            raise ImportError(
+                f"Parent class [{self.parent_fqcn}] not found"
+            ) from None
+
+        return parent_model.objects.get(id=self.parent_id)
 
     def save(self, *args, **kwargs):
+        parent = self.get_parent()
         # when creating
         if self._state.adding:
             if self.status_message is None:
                 self.status_message = self._get_default_status_message()
 
                 # populate latest_instance of activation at creation
-                self.activation.latest_instance = self
+
+                parent.latest_instance = self
         else:
             if not bool(kwargs) or "update_fields" not in kwargs:
                 raise UpdateFieldsRequiredError(
@@ -236,7 +252,7 @@ class RulebookProcess(models.Model):
                 kwargs["update_fields"].append("status_message")
 
         super().save(*args, **kwargs)
-        self.activation.save(update_fields=["latest_instance"])
+        parent.save(update_fields=["latest_instance"])
 
     def _get_default_status_message(self):
         try:
--- src/aap_eda/core/utils/__init__.py
+++ src/aap_eda/core/utils/__init__.py
@@ -11,3 +11,10 @@
 #  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
+
+
+def get_fully_qualified_name(obj: object) -> str:
+    klass = obj.__class__
+    module = klass.__module__
+    name = klass.__name__
+    return f"{module}.{name}"
