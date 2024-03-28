f63eb73a6eb439932dd78a167df00e273ca36210
Hui Song <hsong@redhat.com>
2024-03-28 14:23:15 -0400

    feat: add refs query parameters to retrieve related resources for eda credentials (#781)

--- src/aap_eda/api/filters/activation.py
+++ src/aap_eda/api/filters/activation.py
@@ -33,15 +33,10 @@ class ActivationFilter(django_filters.FilterSet):
         lookup_expr="exact",
         label="Filter by Decision Environment ID.",
     )
-    credential_id = django_filters.NumberFilter(
-        field_name="decision_environment__eda_credential_id",
-        lookup_expr="exact",
-        label="Filter by Credential ID.",
-    )
 
     class Meta:
         model = models.Activation
-        fields = ["name", "status", "decision_environment_id", "credential_id"]
+        fields = ["name", "status", "decision_environment_id"]
 
 
 class ActivationInstanceFilter(django_filters.FilterSet):
