f401e44667389df85b0be500d980bb2b32baa167
Hui Song <hsong@redhat.com>
2024-03-29 16:04:04 -0400

    Add missind validation in is_activation_valid

--- src/aap_eda/api/serializers/activation.py
+++ src/aap_eda/api/serializers/activation.py
@@ -37,7 +37,7 @@ from aap_eda.api.serializers.project import (
 from aap_eda.api.serializers.rulebook import RulebookRefSerializer
 from aap_eda.api.vault import encrypt_string
 from aap_eda.core import models, validators
-from aap_eda.core.enums import CredentialType, ProcessParentType
+from aap_eda.core.enums import DefaultCredentialType, ProcessParentType
 from aap_eda.core.utils.credentials import get_secret_fields
 from aap_eda.core.utils.strings import (
     substitute_extra_vars,
@@ -87,14 +87,16 @@ def _updated_ruleset(validated_data):
 def _handle_eda_credentials(
     validated_data: dict,
     extra_var_obj: models.ExtraVar,
-    vault: models.CredentialType,
     password: str,
 ) -> bool:
     system_vault_credential_needed = False
 
     for eda_credential_id in validated_data["eda_credentials"]:
         eda_credential = models.EdaCredential.objects.get(id=eda_credential_id)
-        if eda_credential.credential_type.id == vault.id:
+        if eda_credential.credential_type.name in [
+            DefaultCredentialType.VAULT,
+            DefaultCredentialType.AAP,
+        ]:
             continue
 
         schema_inputs = eda_credential.credential_type.inputs
@@ -116,16 +118,16 @@ def _handle_eda_credentials(
 
         if extra_var_obj:
             existing_data = yaml.safe_load(extra_var_obj.extra_var)
-            for key in data["extra_vars"]:
+            for key in data.get("extra_vars", []):
                 existing_data[key] = data["extra_vars"][key]
 
             extra_var_obj.extra_var = yaml.dump(existing_data)
             extra_var_obj.save(update_fields=["extra_var"])
         else:
-            extra_var = models.ExtraVar.objects.create(
+            extra_var_obj = models.ExtraVar.objects.create(
                 extra_var=yaml.dump(data["extra_vars"])
             )
-            validated_data["extra_var_id"] = extra_var.id
+            validated_data["extra_var_id"] = extra_var_obj.id
 
     return system_vault_credential_needed
 
@@ -147,7 +149,7 @@ def _create_system_eda_credential(
 
 
 def _get_vault_credential_type():
-    return models.CredentialType.objects.get(name=CredentialType.VAULT)
+    return models.CredentialType.objects.get(name=DefaultCredentialType.VAULT)
 
 
 class ActivationSerializer(serializers.ModelSerializer):
@@ -345,8 +347,8 @@ class ActivationCreateSerializer(serializers.ModelSerializer):
         if not awx_token:
             validate_rulebook_token(data["rulebook_id"])
 
-        if data.get("eda_credentials") and data.get("extra_var_id"):
-            validate_eda_credentails(
+        if data.get("eda_credentials"):
+            validate_eda_credentials(
                 data.get("eda_credentials"), data.get("extra_var_id")
             )
 
@@ -378,7 +380,7 @@ class ActivationCreateSerializer(serializers.ModelSerializer):
 
         if validated_data.get("eda_credentials"):
             system_vault_credential_needed = _handle_eda_credentials(
-                validated_data, extra_var_obj, vault, password
+                validated_data, extra_var_obj, password
             )
 
         if system_vault_credential_needed:
@@ -570,12 +572,23 @@ class PostActivationSerializer(serializers.ModelSerializer):
         validators=[validators.check_if_awx_token_exists],
     )
     rulebook_id = serializers.IntegerField(allow_null=True)
+    eda_credentials = serializers.ListField(
+        required=False,
+        allow_null=True,
+        child=serializers.IntegerField(),
+    )
 
     def validate(self, data):
         awx_token = data.get("awx_token_id")
 
-        if not awx_token:
+        eda_credentials = data.get("eda_credentials", [])
+        aap_cred = _aap_in_eda_credentials(eda_credentials)
+
+        if not awx_token and not aap_cred:
             validate_rulebook_token(data["rulebook_id"])
+
+        if data.get("extra_var_id"):
+            validate_eda_credentials(eda_credentials, data.get("extra_var_id"))
 
         return data
 
@@ -592,6 +605,7 @@ class PostActivationSerializer(serializers.ModelSerializer):
             "modified_at",
             "awx_token_id",
             "rulebook_id",
+            "eda_credentials",
         ]
         read_only_fields = [
             "id",
@@ -613,7 +627,11 @@ def get_rules_count(ruleset_stats: dict) -> tuple[int, int]:
 
 
 def is_activation_valid(activation: models.Activation) -> tuple[bool, str]:
-    serializer = PostActivationSerializer(data=activation.__dict__)
+    data = activation.__dict__
+    data["eda_credentials"] = [
+        obj.id for obj in activation.eda_credentials.all()
+    ]
+    serializer = PostActivationSerializer(data=data)
 
     valid = serializer.is_valid()
     message = parse_validation_errors(serializer.errors)
@@ -646,27 +664,29 @@ def validate_rulebook_token(rulebook_id: Union[int, None]) -> None:
         rulesets_data,
     ):
         raise serializers.ValidationError(
-            "The rulebook requires an Awx Token.",
+            "The rulebook requires an Awx Token or RH AAP credential.",
         )
 
 
-def validate_eda_credentails(
+def validate_eda_credentials(
     eda_credential_ids: list[int], extra_var_id: int
 ) -> None:
+    existing_keys = []
     try:
         vault = _get_vault_credential_type()
-        extra_var = models.ExtraVar.objects.get(id=extra_var_id)
-        existing_data = yaml.safe_load(extra_var.extra_var)
+        if extra_var_id:
+            extra_var = models.ExtraVar.objects.get(id=extra_var_id)
+            existing_data = yaml.safe_load(extra_var.extra_var)
+            existing_keys = [*existing_data.keys()]
     except models.CredentialType.DoesNotExist:
         raise serializers.ValidationError(
-            f"CredentialType with name '{CredentialType.VAULT}' does not exist"
+            f"CredentialType with name '{DefaultCredentialType.VAULT}'"
+            " does not exist"
         )
     except models.ExtraVar.DoesNotExist:
         raise serializers.ValidationError(
             f"ExtraVar with id {extra_var_id} does not exist"
         )
-
-    existing_keys = [*existing_data.keys()]
 
     for eda_credential_id in eda_credential_ids:
         try:
@@ -679,8 +699,8 @@ def validate_eda_credentails(
 
             injectors = eda_credential.credential_type.injectors
 
-            for key in injectors["extra_vars"]:
-                if key in existing_keys or key in existing_data:
+            for key in injectors.get("extra_vars", []):
+                if key in existing_keys:
                     message = (
                         f"Key: {key} already exists in extra var. "
                         f"It conflicts with credential type: "
@@ -694,3 +714,18 @@ def validate_eda_credentails(
             raise serializers.ValidationError(
                 f"EdaCredential with id {eda_credential_id} does not exist"
             )
+
+
+def _aap_in_eda_credentials(eda_credential_ids: list[int]) -> bool:
+    aap_credential_type = models.CredentialType.objects.get(
+        name=DefaultCredentialType.AAP
+    )
+    for eda_credential_id in eda_credential_ids:
+        eda_credential = models.EdaCredential.objects.get(pk=eda_credential_id)
+        if (
+            eda_credential
+            and eda_credential.credential_type.id == aap_credential_type.id
+        ):
+            return True
+
+    return False
--- tests/integration/api/test_activation.py
+++ tests/integration/api/test_activation.py
@@ -825,7 +825,7 @@ def test_create_activation_no_token_but_required(
     response = client.post(f"{api_url_v1}/activations/", data=test_activation)
     assert response.status_code == status.HTTP_400_BAD_REQUEST
     assert (
-        "The rulebook requires an Awx Token."
+        "The rulebook requires an Awx Token or RH AAP credential."
         in response.data["non_field_errors"]
     )
 
@@ -881,7 +881,10 @@ def test_restart_activation_with_required_token_deleted(
         f"{api_url_v1}/activations/{response.data['id']}/restart/",
     )
     assert response.status_code == status.HTTP_400_BAD_REQUEST
-    assert "The rulebook requires an Awx Token." in response.data["errors"]
+    assert (
+        "The rulebook requires an Awx Token or RH AAP credential."
+        in response.data["errors"]
+    )
 
 
 @pytest.mark.django_db
