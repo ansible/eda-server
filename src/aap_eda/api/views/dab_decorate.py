f7b1106fbcffb309a5f8fb278223426f4de27aba
Alan Rominger <arominge@redhat.com>
2024-04-06 08:54:00 -0400

    Correctly apply to the assignment views too

--- src/aap_eda/api/views/dab_decorate.py
+++ src/aap_eda/api/views/dab_decorate.py
@@ -1,14 +1,25 @@
-from ansible_base.rbac.api.serializers import RoleDefinitionSerializer
-from ansible_base.rbac.api.views import RoleDefinitionViewSet
-from drf_spectacular.utils import OpenApiResponse, extend_schema
+from ansible_base.rbac.api import views
+from drf_spectacular.utils import (
+    OpenApiResponse,
+    extend_schema,
+    extend_schema_view,
+)
 from rest_framework import status
 
-extend_schema(
-    description="Create a RoleDefinition.",
-    responses={
-        status.HTTP_201_CREATED: OpenApiResponse(
-            RoleDefinitionSerializer,
-            description="Return a created RoleDefinition.",
+
+for viewset_cls in [
+    views.RoleDefinitionViewSet,
+    views.RoleUserAssignmentViewSet,
+    views.RoleTeamAssignmentViewSet,
+]:
+    extend_schema_view(
+        create=extend_schema(
+            description="Create a RoleDefinition.",
+            responses={
+                status.HTTP_201_CREATED: OpenApiResponse(
+                    viewset_cls.serializer_class,
+                    description="Return a created RoleDefinition.",
+                ),
+            },
         ),
-    },
-)(RoleDefinitionViewSet.create)
+    )(viewset_cls)
