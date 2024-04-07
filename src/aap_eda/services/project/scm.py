72c75b1ea555da254f407735d7178eab14c31744
Madhu Kanoor <mkanoor@redhat.com>
2024-04-07 11:31:13 -0400

    chore: fix the workflow

--- src/aap_eda/services/project/scm.py
+++ src/aap_eda/services/project/scm.py
@@ -249,25 +249,26 @@ class PlaybookExecutor:
         timeout: Optional[float] = 30,
         cwd: Optional[StrPath] = None,
     ):
-        cwd = "/tmp"
-        res = pexpect.run(
-            "git config --get-all --show-origin credential.helper",
-            env={"GIT_PAGER": "cat"},
-            cwd=cwd,
-        )
-        logger.info(f"git credential helper: {res}")
-        res = pexpect.run("git config --unset credential.helper")
-        logger.info(f"Unset credential helper: {res}")
-        res = pexpect.run("ls -l .git")
-        logger.info(f"Git directory: {res.decode()}")
-        logger.info(f"cwd: {os.getcwd()}")
-        logger.info(f"local cwd: {cwd}")
-        res = pexpect.run(
-            "git config --list --show-origin --show-scope",
-            env={"GIT_PAGER": "cat"},
-            cwd=cwd,
-        )
-        logger.info(f"Git config: {res.decode()}")
+
+        #cwd = "/tmp"
+        #res = pexpect.run(
+        #    "git config --get-all --show-origin credential.helper",
+        #    env={"GIT_PAGER": "cat"},
+        #    cwd=cwd,
+        #)
+        #logger.info(f"git credential helper: {res}")
+        #res = pexpect.run("git config --unset credential.helper")
+        #logger.info(f"Unset credential helper: {res}")
+        #res = pexpect.run("ls -l .git")
+        #logger.info(f"Git directory: {res.decode()}")
+        #logger.info(f"cwd: {os.getcwd()}")
+        #logger.info(f"local cwd: {cwd}")
+        #res = pexpect.run(
+        #    "git config --list --show-origin --show-scope",
+        #    env={"GIT_PAGER": "cat"},
+        #    cwd=cwd,
+        #)
+        #logger.info(f"Git config: {res.decode()}")
         if final_url:
             logger.info(f"partial url: {final_url.split('@')[0][:-6]}")
 
