be08fa27f15d1d68d83a3ad62ea9f254f2a1b132
Alan Rominger <arominge@redhat.com>
2024-04-08 09:41:19 -0400

    Add copyright header, handle missing serializer

--- src/aap_eda/api/views/dab_base.py
+++ src/aap_eda/api/views/dab_base.py
@@ -1,3 +1,18 @@
+#  Copyright 2024 Red Hat, Inc.
+#
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+
+from rest_framework.serializers import Serializer
 from rest_framework.views import APIView
 
 registry = {}
@@ -37,6 +52,9 @@ def convert_to_create_serializer(cls):
 
 class BaseAPIView(APIView):
     def get_serializer_class(self):
+        if not hasattr(super(), "get_serializer_class"):
+            # Use base serializer for DAB views that do not give any serializer
+            return Serializer
         serializer_cls = super().get_serializer_class()
         if self.action == "create":
             return convert_to_create_serializer(serializer_cls)
@@ -48,6 +66,8 @@ class BaseAPIView(APIView):
         # in these cases we use a custom creation serializer
         if "context" in kwargs and hasattr(super(), "get_serializer"):
             return super().get_serializer(*args, **kwargs)
+        elif not hasattr(super(), "get_serializer_class"):
+            return None
 
         # If not, we are processing a real request
         # in that case, we do not want the phony and incorrect serializer
