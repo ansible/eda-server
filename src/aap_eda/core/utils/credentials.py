740000271c544603e381eea13105dd9afcabed78
Hui Song <hsong@redhat.com>
2024-05-01 12:42:56 -0400

    feat: add validation on GPG public keys (#874)

--- src/aap_eda/core/utils/credentials.py
+++ src/aap_eda/core/utils/credentials.py
@@ -14,6 +14,7 @@
 
 import re
 
+import gnupg
 import jinja2
 import yaml
 from django.core.exceptions import ValidationError
@@ -119,6 +120,11 @@ def validate_inputs(schema: dict, inputs: dict) -> dict:
                     errors["inputs.ssh_key_unlock"] = result
                 else:
                     errors[display_field] = result
+
+        if field == "gpg_public_key":
+            result = _validate_gpg_public_key(user_input)
+            if bool(result):
+                errors[display_field] = result
 
         if data.get("type") == "boolean":
             if user_input and not isinstance(user_input, bool):
@@ -344,5 +350,21 @@ def _validate_ssh_key(schema: dict, data: str, inputs: dict) -> list[str]:
                 errors.append(PROTECTED_PASSPHRASE_ERROR)
     except ValidationError as e:
         errors.append(str(e))
+
+    return errors
+
+
+def _validate_gpg_public_key(key_data: str) -> list[str]:
+    errors = []
+
+    gpg = gnupg.GPG()
+    import_result = gpg.import_keys(key_data)
+
+    if import_result.returncode != 0:
+        errors.append("No valid GPG data found.")
+        return errors
+
+    if import_result.sec_read > 0:
+        errors.append("Key is not a public key")
 
     return errors
