239cc9c16359b679660a45e0ee330272be0f5511
Joe Shimkus <35382397+jshimkus-rh@users.noreply.github.com>
2024-05-06 17:18:56 -0400

    Additional HS tests (#864)
    Co-authored-by: Doston <31990136+Dostonbek1@users.noreply.github.com>
    Co-authored-by: Bill Wei <bilwei@redhat.com>
    Co-authored-by: Alan Rominger <arominge@redhat.com>
    Co-authored-by: Alex <aizquier@redhat.com>

--- tests/unit/test_orchestrator.py
+++ tests/unit/test_orchestrator.py
@@ -12,18 +12,312 @@
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
 
+import functools
 from datetime import datetime, timedelta
 from unittest import mock
 
 import pytest
+from django.conf import settings
 
 from aap_eda.core.enums import ProcessParentType
-from aap_eda.core.models import Activation, EventStream
+from aap_eda.core.models import Activation, EventStream, RulebookProcess
+from aap_eda.settings import default
+from aap_eda.tasks import orchestrator
 from aap_eda.tasks.orchestrator import (
+    HealthyQueueNotFoundError,
     UnknownProcessParentType,
     check_rulebook_queue_health,
+    get_least_busy_queue_name,
     get_process_parent,
 )
+
+
+# Sets up a collection of mocked queues in specified states for testing
+# orchestrator functionality.
+#
+# Queues are specified via a dictionary using the following format.
+# Any number of queues may be specified.
+#
+# {                                                             # noqa: E800
+#     "queue1": {                                               # noqa: E800
+#         "workers": {                                          # noqa: E800
+#             "worker1_1": {                                    # noqa: E800
+#                 "responsive": True,                           # noqa: E800
+#             },                                                # noqa: E800
+#         },                                                    # noqa: E800
+#         "process_count": 1,                                   # noqa: E800
+#     },                                                        # noqa: E800
+#     "queue2": {                                               # noqa: E800
+#         "workers": {                                          # noqa: E800
+#             "worker2_1": {                                    # noqa: E800
+#                 "responsive": True,                           # noqa: E800
+#             },                                                # noqa: E800
+#         },                                                    # noqa: E800
+#         "process_count": 2,                                   # noqa: E800
+#     },                                                        # noqa: E800
+# }                                                             # noqa: E800
+#
+def _mock_up_queues(monkeypatch, queues):
+    mock_queues = {}
+
+    monkeypatch.setattr(
+        settings, "RULEBOOK_WORKER_QUEUES", list(queues.keys())
+    )
+
+    def _process_object_filter(**kwargs) -> mock.Mock:
+        return mock_queues[kwargs.get("rulebookprocessqueue__queue_name")]
+
+    monkeypatch.setattr(
+        RulebookProcess.objects, "filter", _process_object_filter
+    )
+
+    def _get_queue(name: str) -> mock.Mock:
+        return mock_queues[name]
+
+    monkeypatch.setattr(orchestrator, "get_queue", _get_queue)
+
+    def _worker_all(queue=None) -> list:
+        return queue.workers
+
+    monkeypatch.setattr(orchestrator.Worker, "all", _worker_all)
+
+    def _process_count(queue: mock.Mock) -> None:
+        return queue.process_count
+
+    def _empty_queue(queue: mock.Mock) -> None:
+        queue.process_count = 0
+
+    for queue_name in queues.keys():
+        mock_queue = mock.Mock()
+        mock_queue.name = queue_name
+        mock_queue.process_count = queues[queue_name]["process_count"]
+        mock_queue.count = functools.partial(_process_count, mock_queue)
+        mock_queue.empty = functools.partial(_empty_queue, mock_queue)
+
+        mock_queue.workers = []
+
+        for worker_name in queues[queue_name]["workers"].keys():
+            mock_worker = mock.Mock()
+            mock_worker.name = worker_name
+            mock_worker.responsive = queues[queue_name]["workers"][
+                worker_name
+            ]["responsive"]
+            mock_worker.last_heartbeat = datetime.now()
+            if not mock_worker.responsive:
+                mock_worker.last_heartbeat -= timedelta(
+                    seconds=(2 * default.DEFAULT_WORKER_HEARTBEAT_TIMEOUT)
+                )
+            mock_queue.workers.append(mock_worker)
+
+        mock_queues[queue_name] = mock_queue
+
+    return mock_queues
+
+
+@pytest.fixture
+def one_queue(monkeypatch):
+    return _mock_up_queues(
+        monkeypatch,
+        {
+            "queue": {
+                "workers": {
+                    "worker": {"responsive": True},
+                },
+                "process_count": 1,
+            },
+        },
+    )
+
+
+@pytest.fixture
+def two_queues_differing_counts(monkeypatch):
+    return _mock_up_queues(
+        monkeypatch,
+        {
+            "queue1": {
+                "workers": {
+                    "worker1_1": {"responsive": True},
+                },
+                "process_count": 1,
+            },
+            "queue2": {
+                "workers": {
+                    "worker2_1": {"responsive": True},
+                },
+                "process_count": 2,
+            },
+        },
+    )
+
+
+@pytest.fixture
+def two_queues_one_responsive(monkeypatch):
+    return _mock_up_queues(
+        monkeypatch,
+        {
+            "queue1": {
+                "workers": {
+                    "worker1_1": {"responsive": True},
+                },
+                "process_count": 1,
+            },
+            "queue2": {
+                "workers": {
+                    "worker2_1": {"responsive": False},
+                },
+                "process_count": 1,
+            },
+        },
+    )
+
+
+@pytest.fixture
+def two_queues_one_responsive_higher_count(monkeypatch):
+    return _mock_up_queues(
+        monkeypatch,
+        {
+            "queue1": {
+                "workers": {
+                    "worker1_1": {"responsive": True},
+                },
+                "process_count": 2,
+            },
+            "queue2": {
+                "workers": {
+                    "worker2_1": {"responsive": False},
+                },
+                "process_count": 1,
+            },
+        },
+    )
+
+
+@pytest.fixture
+def two_queues_neither_responsive(monkeypatch):
+    return _mock_up_queues(
+        monkeypatch,
+        {
+            "queue1": {
+                "workers": {
+                    "worker1_1": {"responsive": False},
+                },
+                "process_count": 1,
+            },
+            "queue2": {
+                "workers": {
+                    "worker2_1": {"responsive": False},
+                },
+                "process_count": 1,
+            },
+        },
+    )
+
+
+@pytest.mark.parametrize(
+    "fixture",
+    [
+        "one_queue",
+        "two_queues_differing_counts",
+        "two_queues_one_responsive",
+        "two_queues_one_responsive_higher_count",
+        "two_queues_neither_responsive",
+    ],
+)
+def test_get_least_busy_queue_name(fixture, request):
+    queues = request.getfixturevalue(fixture)
+
+    responsive_queues = [
+        queue
+        for queue in queues.values()
+        if sum(1 for _ in filter(lambda w: w.responsive, queue.workers)) > 0
+    ]
+
+    if responsive_queues:
+        expected_queue = min(responsive_queues, key=lambda q: q.count())
+        assert get_least_busy_queue_name() == expected_queue.name
+    else:
+        with pytest.raises(HealthyQueueNotFoundError):
+            get_least_busy_queue_name()
+
+
+@pytest.fixture
+def multi_queue_various_states(monkeypatch):
+    return _mock_up_queues(
+        monkeypatch,
+        {
+            "good1": {
+                "workers": {
+                    "good1_1": {"responsive": True},
+                },
+                "process_count": 1,
+            },
+            "good2": {
+                "workers": {
+                    "good2_1": {"responsive": True},
+                    "good2_2": {"responsive": True},
+                    "good2_3": {"responsive": False},
+                },
+                "process_count": 1,
+            },
+            "good3": {
+                "workers": {
+                    "good3_1": {"responsive": False},
+                    "good3_2": {"responsive": False},
+                    "good3_3": {"responsive": True},
+                },
+                "process_count": 1,
+            },
+            "bad1": {
+                "workers": {
+                    "bad1_1": {"responsive": False},
+                },
+                "process_count": 1,
+            },
+            "bad2": {
+                "workers": {
+                    "bad2_1": {"responsive": False},
+                    "bad2_2": {"responsive": False},
+                    "bad2_3": {"responsive": False},
+                },
+                "process_count": 1,
+            },
+        },
+    )
+
+
+def test_check_rulebook_queue_health(multi_queue_various_states):
+    queues = multi_queue_various_states
+
+    expected_good_queues = [
+        queue
+        for queue in queues.values()
+        if sum(1 for _ in filter(lambda w: w.responsive, queue.workers)) > 0
+    ]
+    expected_bad_queues = [
+        queue
+        for queue in queues.values()
+        if sum(1 for _ in filter(lambda w: w.responsive, queue.workers)) == 0
+    ]
+
+    found_good_queues = []
+    found_bad_queues = []
+    for queue in queues.values():
+        if check_rulebook_queue_health(queue.name):
+            found_good_queues.append(queue)
+        else:
+            found_bad_queues.append(queue)
+
+    assert len(found_good_queues) == len(expected_good_queues)
+    assert len(
+        [x for x in expected_good_queues if x in found_good_queues]
+    ) == len(expected_good_queues)
+
+    assert len(found_bad_queues) == len(expected_bad_queues)
+    assert len(
+        [x for x in expected_bad_queues if x in found_bad_queues]
+    ) == len(expected_bad_queues)
+    for queue in found_bad_queues:
+        assert queue.count() == 0
 
 
 def test_get_process_parent_activation():
