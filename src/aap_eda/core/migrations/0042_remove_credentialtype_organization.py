9d9854040df1fff814a9629558b3d413b1537942
Alex <aizquier@redhat.com>
2024-08-01 18:39:16 +0200

    sync migrations
    Signed-off-by: Alex <aizquier@redhat.com>

--- src/aap_eda/core/migrations/0042_remove_credentialtype_organization.py
+++ src/aap_eda/core/migrations/0042_remove_credentialtype_organization.py
@@ -5,7 +5,7 @@ from django.db import migrations
 
 class Migration(migrations.Migration):
     dependencies = [
-        ("core", "0040_remove_rulebookprocesslog_line_number"),
+        ("core", "0041_alter_credentialtype_options_and_more"),
     ]
 
     operations = [
