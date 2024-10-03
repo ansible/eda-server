b64319343fc6ea823a4f1208ee950de9492a7b58
Hui Song <hsong@redhat.com>
2024-10-03 12:21:47 -0400

    validate the event_stream_type with credential_type in creating event streams

--- tests/integration/api/test_event_stream_basic.py
+++ tests/integration/api/test_event_stream_basic.py
@@ -58,6 +58,7 @@ def test_post_event_stream_with_basic_auth(
     data_in = {
         "name": "test-es-1",
         "eda_credential_id": obj["id"],
+        "event_stream_type": obj["credential_type"]["kind"],
         "organization_id": get_default_test_org().id,
         "test_mode": True,
     }
