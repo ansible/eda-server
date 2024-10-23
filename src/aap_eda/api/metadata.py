895ec63ae74f68beadef5dbeb9b236853a6fa84a
Bill Wei <bilwei@redhat.com>
2024-10-23 15:28:39 -0400

    feat: enhance options for /settings/system/ API (#1108)

--- src/aap_eda/api/metadata.py
+++ src/aap_eda/api/metadata.py
@@ -1,11 +1,34 @@
 from django.core.exceptions import PermissionDenied
 from django.http import Http404
+from django.utils.encoding import force_str
 from rest_framework import exceptions, metadata
 from rest_framework.request import clone_request
 
+ADDITIONAL_ATTRS = [
+    "min_length",
+    "max_length",
+    "min_value",
+    "max_value",
+    "category",
+    "category_slug",
+    "defined_in_file",
+    "unit",
+    "hidden",
+    "default",
+]
+
 
 class EDAMetadata(metadata.SimpleMetadata):
-    """Overwritten to show PATCH in OPTIONS."""
+    """Overwritten to show PATCH in OPTIONS and add more attributes."""
+
+    def get_field_info(self, field):
+        field_info = super().get_field_info(field)
+
+        for attr in ADDITIONAL_ATTRS:
+            value = getattr(field, attr, None)
+            if value is not None and value != "":
+                field_info[attr] = force_str(value, strings_only=True)
+        return field_info
 
     def determine_actions(self, request, view):
         """For generic class based views we return information about.
@@ -30,8 +53,39 @@ class EDAMetadata(metadata.SimpleMetadata):
                 # If user has appropriate permissions for the view, include
                 # appropriate metadata about the fields that should be supplied
                 serializer = view.get_serializer()
-                actions[method] = self.get_serializer_info(serializer)
+                action = self.get_serializer_info(serializer)
+                EDAMetadata._customize_field_attributes(method, action)
+                actions[method] = action
             finally:
                 view.request = request
 
         return actions
+
+    @staticmethod
+    def _customize_field_attributes(method: str, action: dict):
+        for field, meta in list(action.items()):
+            if not isinstance(meta, dict):
+                continue
+
+            # For GET method, remove meta attributes that aren't relevant
+            # when reading a field and remove write-only fields.
+            if method == "GET":
+                attrs_to_remove = (
+                    "required",
+                    "read_only",
+                    "default",
+                    "min_length",
+                    "max_length",
+                )
+                for attr in attrs_to_remove:
+                    meta.pop(attr, None)
+                if meta.pop("write_only", False):
+                    action.pop(field)
+
+            # For PUT/PATCH/POST methods, remove read-only fields.
+            if method in ("PUT", "PATCH", "POST"):
+                # file-based read-only settings can't be updated
+                meta.pop("defined_in_file", False)
+
+                if meta.pop("read_only", False):
+                    action.pop(field)
