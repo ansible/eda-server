a579210879d67d02b962f2d97259c88d61bfac57
Zack Kayyali <zkayyali812@gmail.com>
2025-01-29 15:24:32 -0500

    use 'feature_flag_state/' endpoint

--- tests/integration/api/test_feature_flags.py
+++ tests/integration/api/test_feature_flags.py
@@ -7,7 +7,7 @@ from tests.integration.constants import api_url_v1
 
 @pytest.mark.django_db
 def test_feature_flags_list_endpoint(admin_client):
-    response = admin_client.get(f"{api_url_v1}/feature_flags_definition/")
+    response = admin_client.get(f"{api_url_v1}/feature_flags_state/")
     assert response.status_code == status.HTTP_200_OK, response.data
     # Test number of feature flags.
     # Modify each time a flag is added to default settings
@@ -19,14 +19,17 @@ def test_feature_flags_list_endpoint(admin_client):
         "FEATURE_SOME_PLATFORM_FLAG_ENABLED": [
             {"condition": "boolean", "value": False, "required": True},
             {"condition": "before date", "value": "2022-06-01T12:00Z"},
-        ]
+        ],
+        "FEATURE_SOME_PLATFORM_FLAG_FOO_ENABLED": [
+            {"condition": "boolean", "value": True},
+            {"condition": "before date", "value": "2022-06-01T12:00Z"},
+        ],
     }
 )
 @pytest.mark.django_db
 def test_feature_flags_override_flags(admin_client):
-    response = admin_client.get(f"{api_url_v1}/feature_flags_definition/")
+    response = admin_client.get(f"{api_url_v1}/feature_flags_state/")
     assert response.status_code == status.HTTP_200_OK, response.data
-    assert len(response.data) == 1  # Validates number of feature flags
-    assert (
-        len(response.data["FEATURE_SOME_PLATFORM_FLAG_ENABLED"]) == 2
-    )  # Validates number of conditions
+    assert len(response.data) == 2  # Validates number of feature flags
+    assert response.data["FEATURE_SOME_PLATFORM_FLAG_ENABLED"] is False
+    assert response.data["FEATURE_SOME_PLATFORM_FLAG_FOO_ENABLED"] is True
