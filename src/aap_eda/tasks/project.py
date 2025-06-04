ansible/eda-server

commit 1d738c425c49a8068aea20b3112782588ab2b4cc

wip: upgrade dispatcher, projects and activations working.  (#1296)
<!-- Mandatory: Provide a clear, concise description of the changes and
their purpose -->
<!-- If applicable, provide a link to the issue that is being addressed
-->

<!-- What is being changed? -->
<!-- Why is this change needed? -->
<!-- How does this change address the issue? -->
<!-- Does this change introduce any new dependencies, blockers or
breaking changes? -->
<!-- How it can be tested? -->

---------

Signed-off-by: Alex <aizquier@redhat.com>

src/aap_eda/tasks/project.py
@@ -13,12 +13,14 @@
 #  limitations under the License.
 
 import logging
+import typing as tp
 
 import django_rq
 from ansible_base.lib.utils.db import advisory_lock
-from dispatcherd.publish import task
+from dispatcherd.publish import submit_task
 from django.conf import settings
 
+from aap_eda import utils
 from aap_eda.core import models, tasking
 from aap_eda.services.project import ProjectImportError, ProjectImportService
 from aap_eda.settings import features
@@ -33,7 +35,7 @@ PROJECT_TASKS_QUEUE = "default"
 job = tasking.redis_connect_retry()(django_rq.job)
 
 
-def import_project(project_id: int):
+def import_project(project_id: int) -> tp.Optional[str]:
     """Import project async task.
 
     Proxy for import_project_dispatcherd and import_project_rq.
@@ -43,25 +45,28 @@ def import_project(project_id: int):
             logger.debug(
                 f"Another task already importing project {project_id}, exiting"
             )
-            return
+            return None
         if features.DISPATCHERD:
-            # task decorator is equivalent a "job" decorator for dispatcherd
-            # TODO: move to submit_task (preferred way to submit tasks)
-            # Now we keep the decorator to take advantage of the "delay" method
-            # and limit changes to the code for the implementation of dispatcherd
-            # under a feature flag.
-            logger.info("dispatcherd1")
-            job_data, _ = task(queue=PROJECT_TASKS_QUEUE)(
-                _import_project
-            ).delay(project_id=project_id)
-            job_id = job_data["uuid"]
-            return job_id
+            return import_project_dispatcherd(project_id)
+
         # rq
-        job_data = job(PROJECT_TASKS_QUEUE)(_import_project).delay(
-            project_id=project_id
-        )
-        job_id = job_data.id
-        return job_id
+        job_data = import_project_rq.delay(project_id=project_id)
+        return job_data.id
+
+
+def import_project_dispatcherd(project_id: int) -> str:
+    queue_name = utils.sanitize_postgres_identifier(PROJECT_TASKS_QUEUE)
+    job_data, queue = submit_task(
+        _import_project,
+        queue=queue_name,
+        args=(project_id,),
+    )
+    return job_data["uuid"]
+
+
+@job(PROJECT_TASKS_QUEUE)
+def import_project_rq(project_id: int):
+    _import_project(project_id)
 
 
 def _import_project(project_id: int):
@@ -80,42 +85,37 @@ def _import_project(project_id: int):
     logger.info(f"Task complete: Import project ( project_id={project.id} )")
 
 
-def sync_project(project_id: int):
+def sync_project(project_id: int) -> tp.Optional[str]:
     """Sync project async task.
 
     Proxy for sync_project_dispatcherd and sync_project_rq.
     """
-    if features.DISPATCHERD:
-        return sync_project_dispatcherd(project_id)
-
-    return sync_project_rq(project_id)
-
-
-# task decorator is equivalent a "job" decorator for dispatcherd
-# TODO: move to submit_task (preferred way to submit tasks)
-# Now we keep the decorator to take advantage of the "delay" method
-# and limit changes to the code for the implementation of dispatcherd
-# under a feature flag.
-@task(queue=PROJECT_TASKS_QUEUE)
-def sync_project_dispatcherd(project_id: int):
-    with advisory_lock(f"import_project_{project_id}", wait=False) as acquired:
+    with advisory_lock(f"sync_project_{project_id}", wait=False) as acquired:
         if not acquired:
             logger.debug(
                 f"Another task already syncing project {project_id}, exiting"
             )
-            return
-        _sync_project(project_id)
+            return None
+        if features.DISPATCHERD:
+            return sync_project_dispatcherd(project_id)
+        # rq
+        job_data = sync_project_rq.delay(project_id=project_id)
+        return job_data.id
+
+
+def sync_project_dispatcherd(project_id: int) -> str:
+    queue_name = utils.sanitize_postgres_identifier(PROJECT_TASKS_QUEUE)
+    job_data, queue = submit_task(
+        _sync_project,
+        queue=queue_name,
+        args=(project_id,),
+    )
+    return job_data["uuid"]
 
 
 @job(PROJECT_TASKS_QUEUE)
 def sync_project_rq(project_id: int):
-    with advisory_lock(f"import_project_{project_id}", wait=False) as acquired:
-        if not acquired:
-            logger.debug(
-                f"Another task already syncing project {project_id}, exiting"
-            )
-            return
-        _sync_project(project_id)
+    _sync_project(project_id)
 
 
 def _sync_project(project_id: int):

src/aap_eda/api/views/project.py
@@ -33,7 +33,6 @@ from aap_eda.api import exceptions as api_exc, filters, serializers
 from aap_eda.core import models
 from aap_eda.core.enums import Action
 from aap_eda.core.utils import logging_utils
-from aap_eda.settings import features
 
 from .mixins import RedisDependencyMixin, ResponseSerializerMixin
 
@@ -140,7 +139,6 @@ class ProjectViewSet(
         except redis.ConnectionError:
             return RedisDependencyMixin.redis_unavailable_response()
 
-        # Atomically update `import_task_id` field only.
         models.Project.objects.filter(pk=project.id).update(
             import_task_id=job_id
         )
@@ -275,13 +273,18 @@ class ProjectViewSet(
         self.redis_is_available()
 
         try:
-            job_id = sync_project(project.id)
+            job_id = tasks.sync_project(project.id)
         except redis.ConnectionError:
             return RedisDependencyMixin.redis_unavailable_response()
 
         project.import_state = models.Project.ImportState.PENDING
-        project.import_task_id = job_id
-        project.import_error = None
+
+        # job_id can be none if there is already a task running.
+        # this is unlikely since we check the state above
+        # but safety first
+        if job_id:
+            project.import_task_id = job_id
+
         project.save()
 
         logger.info(
@@ -296,20 +299,3 @@ class ProjectViewSet(
 
         serializer = serializers.ProjectSerializer(project)
         return Response(status=status.HTTP_202_ACCEPTED, data=serializer.data)
-
-
-def sync_project(project_id: int) -> str:
-    """
-    Proxy for sync task call for rq and dispatcherd.
-
-    Although the api .delay() is the same for both, the
-    response is different.
-    """
-    if features.DISPATCHERD:
-        job, _ = tasks.sync_project.delay(project_id=project_id)
-        job_id = job["uuid"]
-    else:  # rq
-        job = tasks.sync_project.delay(project_id=project_id)
-        job_id = job.id
-
-    return job_id

pyproject.toml
@@ -70,7 +70,7 @@ validators = "^0.34.0"
 django-flags = "^5.0.13"
 insights-analytics-collector = "^0.3.2"
 distro = "^1.9.0"
-dispatcherd = { version = "v2025.04.30", extras = ["pg_notify", "metrics"] }
+dispatcherd = { version = "v2025.05.12", extras = ["pg_notify", "metrics"] }
 
 
 [tool.poetry.group.test.dependencies]

src/aap_eda/core/tasking/__init__.py
@@ -25,6 +25,7 @@ from dispatcherd.publish import submit_task
 from django.conf import settings
 from rq import results as rq_results
 
+from aap_eda import utils
 from aap_eda.settings import (
     core as core_settings,
     features,
@@ -626,9 +627,7 @@ def unique_enqueue(
     dispatcherd we just enqueue the job.
     """
     if features.DISPATCHERD:
-        return enqueue_job_dispatcherd(
-            queue_name=queue_name, job_id=job_id, *args, **kwargs
-        )
+        return enqueue_job_dispatcherd(queue_name, job_id, *args, **kwargs)
     return unique_enqueue_rq(queue_name, job_id, *args, **kwargs)
 
 
@@ -670,7 +669,7 @@ def enqueue_delay_dispatcherd(
         fn,
         args=args,
         kwargs=kwargs,
-        queue=queue_name,
+        queue=utils.sanitize_postgres_identifier(queue_name),
         uuid=job_id,
         processor_options=[Delayer.Params(delay=delay)],
     )
@@ -736,6 +735,6 @@ def enqueue_job_dispatcherd(
         fn,
         args=args,
         kwargs=kwargs,
-        queue=queue_name,
+        queue=utils.sanitize_postgres_identifier(queue_name),
         uuid=job_id,
     )

src/aap_eda/services/project/imports.py
@@ -65,6 +65,7 @@ def _project_import_wrapper(
         try:
             func(self, project)
             project.import_state = models.Project.ImportState.COMPLETED
+            project.import_error = error
         except ProjectImportWarning as e:
             # import status should show completed but with an error message
             project.import_state = models.Project.ImportState.COMPLETED

src/aap_eda/settings/post_load.py
@@ -537,7 +537,11 @@ def post_loading(loaded_settings: Dynaconf):
                 **settings.DISPATCHERD_DEFAULT_SETTINGS["brokers"][
                     "pg_notify"
                 ],
-                "channels": [settings.RULEBOOK_QUEUE_NAME.replace("-", "_")],
+                "channels": [
+                    utils.sanitize_postgres_identifier(
+                        settings.RULEBOOK_QUEUE_NAME,
+                    )
+                ],
             },
         },
     }

src/aap_eda/tasks/analytics.py
@@ -20,6 +20,7 @@ from ansible_base.lib.utils.db import advisory_lock
 from aap_eda.analytics import collector, utils
 from aap_eda.core import tasking
 from aap_eda.settings import features
+from aap_eda.utils import sanitize_postgres_identifier
 
 logger = logging.getLogger(__name__)
 
@@ -83,6 +84,7 @@ def reschedule_gather_analytics_rq(
 def reschedule_gather_analytics_dispatcherd(
     queue_name: str = ANALYTICS_TASKS_QUEUE,
 ) -> None:
+    queue_name = sanitize_postgres_identifier(queue_name)
     schedule_gather_analytics(queue_name, cancel=True)
 
 

src/aap_eda/tasks/orchestrator.py
@@ -26,6 +26,7 @@ from django.conf import settings
 from django.core.exceptions import ObjectDoesNotExist
 
 import aap_eda.tasks.activation_request_queue as requests_queue
+from aap_eda import utils
 from aap_eda.core import models, tasking
 from aap_eda.core.enums import (
     ActivationRequest,
@@ -476,7 +477,7 @@ def check_rulebook_queue_health_dispatcherd(queue_name: str) -> bool:
 
     """
     ctl = get_control_from_settings(
-        default_publish_channel=queue_name.replace("-", "_")
+        default_publish_channel=utils.sanitize_postgres_identifier(queue_name)
     )
     alive = ctl.control_with_reply("alive")
     if not alive:

commit 15a8daea89d9015ba70c3018db9c71daa8eb0b09

fix: run advisory_lock import/sync project in the right place AAP-47111 (#1333)
Fix a bug introduced in https://github.com/ansible/eda-server/pull/1296
where we are running under the advisory lock not the real import/sync
task but the proxy that schedules the job for rq and dispatcherd.

This fix run the task under the lock in the right place. Also allows to
merge both test_job_uniqueness and test_project_job_uniqueness tests

Jira: AAP-47111

Signed-off-by: Alex <aizquier@redhat.com>

src/aap_eda/tasks/project.py
@@ -28,27 +28,18 @@ from aap_eda.settings import features
 logger = logging.getLogger(__name__)
 PROJECT_TASKS_QUEUE = "default"
 
-# Wrap the django_rq job decorator so its processing is within our retry
-# code.
-
 
 def import_project(project_id: int) -> tp.Optional[str]:
     """Import project async task.
 
     Proxy for import_project_dispatcherd and import_project_rq.
     """
-    with advisory_lock(f"import_project_{project_id}", wait=False) as acquired:
-        if not acquired:
-            logger.debug(
-                f"Another task already importing project {project_id}, exiting"
-            )
-            return None
-        if features.DISPATCHERD:
-            return import_project_dispatcherd(project_id)
+    if features.DISPATCHERD:
+        return import_project_dispatcherd(project_id)
 
-        # rq
-        job_data = import_project_rq(project_id)
-        return job_data
+    # rq
+    job_data = import_project_rq(project_id)
+    return job_data
 
 
 def import_project_dispatcherd(project_id: int) -> str:
@@ -68,6 +59,17 @@ def import_project_rq(project_id: int) -> str:
 
 
 def _import_project(project_id: int):
+    """Wrap _import_project_no_lock with advisory lock."""
+    with advisory_lock(f"import_project_{project_id}", wait=False) as acquired:
+        if not acquired:
+            logger.debug(
+                f"Another task already importing project {project_id}, exiting"
+            )
+            return
+        _import_project_no_lock(project_id)
+
+
+def _import_project_no_lock(project_id: int):
     """Import project without lock.
 
     This function is intended to be run by the tasking system inside the lock.
@@ -88,17 +90,11 @@ def sync_project(project_id: int) -> tp.Optional[str]:
 
     Proxy for sync_project_dispatcherd and sync_project_rq.
     """
-    with advisory_lock(f"sync_project_{project_id}", wait=False) as acquired:
-        if not acquired:
-            logger.debug(
-                f"Another task already syncing project {project_id}, exiting"
-            )
-            return None
-        if features.DISPATCHERD:
-            return sync_project_dispatcherd(project_id)
-        # rq
-        job_data = sync_project_rq(project_id)
-        return job_data
+    if features.DISPATCHERD:
+        return sync_project_dispatcherd(project_id)
+    # rq
+    job_data = sync_project_rq(project_id)
+    return job_data
 
 
 def sync_project_dispatcherd(project_id: int) -> str:
@@ -118,6 +114,21 @@ def sync_project_rq(project_id: int) -> str:
 
 
 def _sync_project(project_id: int):
+    """Wrap _sync_project_no_lock with advisory lock."""
+    with advisory_lock(f"sync_project_{project_id}", wait=False) as acquired:
+        if not acquired:
+            logger.debug(
+                f"Another task already syncing project {project_id}, exiting"
+            )
+            return
+        _sync_project_no_lock(project_id)
+
+
+def _sync_project_no_lock(project_id: int):
+    """Sync project without lock.
+
+    This function is intended to be run by the tasking system inside the lock.
+    """
     logger.info(f"Task started: Sync project ( {project_id=} )")
 
     project = models.Project.objects.get(pk=project_id)

tests/integration/test_advisory_lock.py
@@ -29,11 +29,25 @@ from tests.integration.utils import ThreadSafeList
             "fn_call": "monitor_project_tasks",
             "fn_args": [],
         },
+        {
+            "module_path": "aap_eda.tasks.project",
+            "fn_mock": "_import_project_no_lock",
+            "fn_call": "_import_project",
+            "fn_args": [1],
+        },
+        {
+            "module_path": "aap_eda.tasks.project",
+            "fn_mock": "_sync_project_no_lock",
+            "fn_call": "_sync_project",
+            "fn_args": [1],
+        },
     ],
     ids=[
         "gather_analytics",
         "monitor_rulebook_processes",
         "monitor_project_tasks",  # only scheduled in RQ
+        "_import_project",
+        "_sync_project",
     ],
 )
 @pytest.mark.django_db
@@ -52,81 +66,6 @@ def test_job_uniqueness(module_data):
         with mock.patch(
             f"{module_data['module_path']}.{module_data['fn_mock']}",
         ) as fn_mock:
-
-            def record_call(void=None):
-                time.sleep(1)
-                shared_list.append("called")
-
-            fn_mock.side_effect = record_call
-            try:
-                getattr(module, module_data["fn_call"])(
-                    *module_data["fn_args"],
-                )
-            except Exception as e:
-                pytest.fail(
-                    f"Exception raised in {module_data['fn_call']}: {e}"
-                )
-
-    def thread_safe_call():
-        _wrapper_call(ThreadSafeList(call_log, lock))
-
-    with ThreadPoolExecutor(max_workers=2) as executor:
-        futures = [
-            executor.submit(thread_safe_call),
-            executor.submit(thread_safe_call),
-        ]
-        wait(futures, timeout=3)
-
-    assert len(call_log) == 1
-
-
-@pytest.mark.parametrize(
-    "module_data",
-    [
-        {
-            "module_path": "aap_eda.tasks.project",
-            "fn_mock_dispatcherd": "import_project_dispatcherd",
-            "fn_mock_rq": "import_project_rq",
-            "fn_call": "import_project",
-            "fn_args": [1],
-        },
-        {
-            "module_path": "aap_eda.tasks.project",
-            "fn_mock_dispatcherd": "sync_project_dispatcherd",
-            "fn_mock_rq": "sync_project_rq",
-            "fn_call": "sync_project",
-            "fn_args": [1],
-        },
-    ],
-    ids=[
-        "import_project",
-        "sync_project",
-    ],
-)
-@pytest.mark.django_db
-def test_project_job_uniqueness(module_data):
-    call_log = []
-    lock = Lock()
-
-    def _wrapper_call(shared_list):
-        """Patch fn_mock in a thread-safe way inside each thread."""
-        import importlib
-
-        from aap_eda.settings import features
-
-        module = importlib.import_module(module_data["module_path"])
-
-        importlib.reload(module)
-
-        mock_fn_target = (
-            f"{module_data['fn_mock_dispatcherd']}"
-            if features.DISPATCHERD
-            else f"{module_data['fn_mock_rq']}"
-        )
-
-        mock_path = f"{module_data['module_path']}.{mock_fn_target}"
-
-        with mock.patch(mock_path) as fn_mock:
 
             def record_call(void=None):
                 time.sleep(1)